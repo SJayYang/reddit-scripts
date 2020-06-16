1ca3a06a0670ae9092db5e388523bae38e71d9de
SJayYang <stanley.j.yang@gmail.com>
2020-06-16 19:16:38 -0400

    master

--- fmfbot.py
+++ fmfbot.py
@@ -12,22 +12,24 @@ USER_AGENT = "macos:fmfbot:v0.1.0 (by /u/SJayYang)"
 reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent = USER_AGENT, username=USERNAME, password=PASSWORD)
 
 fmf = reddit.subreddit("frugalmalefashion")
+
 jcrew_query = "j.crew OR jcrew OR Jcrew OR J.crew"
 bono_query = "Bonobos OR bonobos"
 
 
 
-def print_links(subreddit, query):
+def print_links(subreddit, query, reddit, time):
     """
     Subreddit: subreddit instance
     query: expicit query defined
     """
     print("Subreddit: " + str(subreddit))
-    for result in subreddit.search(query, sort="new"):
-        print("reddit.com/r/" + str(subreddit) + "/comments/" + str(result) + "      " + result.title)
+    for result in subreddit.search(query, sort="new", time_filter=time):
+        submission = praw.models.Submission(reddit=reddit, id=str(result))
+        pprint.pprint(submission.title + "   :" + "reddit.com/r/" + str(subreddit) + "/comments/" + str(result) )
 
-print_links(fmf, jcrew_query)
+print_links(fmf, jcrew_query, reddit, "day")
 
 print("\n")
 
-print_links(fmf, bono_query)
+print_links(fmf, bono_query, reddit, "day")
