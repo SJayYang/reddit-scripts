d607c378481bd9044fa0964a18d9e6a81d8da167
SJayYang <stanley.j.yang@gmail.com>
2020-06-16 13:41:54 -0400

    working search filter

--- fmfbot.py
+++ fmfbot.py
@@ -1,13 +1,33 @@
 import praw
-
+import pprint
 
 CLIENT_ID = "m3zlWjWQ9Ahw-w"
-CLIENT_SECRET = "	_j-spOhcwzl-uTPGy_nPdg_eP40"
-USERNAME = input("Username: ")
+CLIENT_SECRET = "_j-spOhcwzl-uTPGy_nPdg_eP40"
+# USERNAME = input("Username: ")
 PASSWORD = input("Password: ")
+USERNAME = "SJayYang"
 USER_AGENT = "macos:fmfbot:v0.1.0 (by /u/SJayYang)"
 
 
 reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent = USER_AGENT, username=USERNAME, password=PASSWORD)
 
-print(reddit.read_only())
+fmf = reddit.subreddit("frugalmalefashion")
+jcrew_query = "j.crew OR jcrew OR Jcrew OR J.crew"
+bono_query = "Bonobos OR bonobos"
+
+
+
+def print_links(subreddit, query):
+    """
+    Subreddit: subreddit instance
+    query: expicit query defined
+    """
+    print("Subreddit: " + str(subreddit))
+    for result in subreddit.search(query, sort="new"):
+        print("reddit.com/r/" + str(subreddit) + "/comments/" + str(result) + "      " + result.title)
+
+print_links(fmf, jcrew_query)
+
+print("\n")
+
+print_links(fmf, bono_query)
